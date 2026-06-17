0df3fb877059e4710e2efc40d8a941ffbf2ef42e
VictoriaCM7 <vichavesma@est.utn.ac.cr>
2026-06-16 19:46:28 -0600

    Prototipo inicial: Se comienza a trabajar en el menú principal

---
+++ js/principal.js
@@ -0,0 +1,15 @@
+const menuItems = document.querySelectorAll(".menu-item");
+
+menuItems.forEach(item => {
+
+    item.addEventListener("click", () => {
+
+        menuItems.forEach(i =>
+            i.classList.remove("active")
+        );
+
+        item.classList.add("active");
+
+    });
+
+});
